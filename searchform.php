72a25a23c28937d45a3889b9696403ffc4a44b11
Ritesh Sanap <riteshsanap@gmail.com>
2015-11-20 18:03:24 +0530

    Fixed search action

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form class="pure-form searchform" action="/" method="get">
+<form class="pure-form searchform" action="<?php echo esc_url(home_url()); ?>" method="get">
     <input type="search" name="s" class="searchbox" placeholder="<?php _e('Search...', 'simply-pure')?>">
     <button type="submit" class="searchbutton"><?php _e('Go', 'simply-pure');?></button>
 </form>

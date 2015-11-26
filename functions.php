51f0d8d8f609978c62d553ed533d16b8d4eff995
Ritesh Sanap <riteshsanap@gmail.com>
2015-11-26 14:54:55 +0530

    Fixed title returning twice

--- functions.php
+++ functions.php
@@ -191,44 +191,6 @@ function simply_pure_categories() {
 	}
 
 }
-
-/**
- * Create a nicely formatted and more specific title element text for output
- * in head of document, based on current view.
- *
- * @version 1.0
- * @since Simply Pure 1.0
- * @author Ritesh Sanap <riteshsanap@gmail.com>
- * 
- * @param string $title Default title text for current view.
- * @param string $sep Optional separator.
- * @return string The filtered title.
- */
-
-function simply_pure_wp_title( $title, $sep ) {
-	global $paged, $page;
-
-	if ( is_feed() ) {
-		return $title;
-	}
-
-	// Add the site name.
-	$title .= get_bloginfo( 'name', 'display' );
-
-	// Add the site description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) ) {
-		$title = "$title $sep $site_description";
-	}
-
-	// Add a page number if necessary.
-	if ( $paged >= 2 || $page >= 2 ) {
-		$title = "$title $sep " . sprintf( __( 'Page %s', 'simply-pure' ), max( $paged, $page ) );
-	}
-
-	return $title;
-}
-add_filter( 'wp_title', 'simply_pure_wp_title', 10, 2 );
 /**
  * Counts the number of sidebars with atleast a Single Widget
  * for more information visit : 
